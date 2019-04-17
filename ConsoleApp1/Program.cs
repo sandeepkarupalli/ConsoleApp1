69ee5a2ff589ea080c570eb10515ed0f37e4af18
Sandeep K <Sandeep K@SANDEEPK-421>
2019-04-17 17:12:45 +0530

    new changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             for(int i=1;i<=10;i++)
             {
-                Console.WriteLine(i*1);
+                Console.WriteLine(i*2);
             }
             Console.ReadLine();
         }
