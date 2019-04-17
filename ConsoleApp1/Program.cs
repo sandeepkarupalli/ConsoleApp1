a3ba02f1aae17c3e7cd6bedf52aad9611d405bc6
Sandeep K <Sandeep K@SANDEEPK-421>
2019-04-17 17:28:34 +0530

    comitted

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             for(int i=1;i<=10;i++)
             {
-                Console.WriteLine(i*3);
+                Console.WriteLine(i*4);
             }
             Console.ReadLine();
         }
